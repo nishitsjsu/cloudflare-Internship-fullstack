0040c72574b1b44063e89735941a93a550d71719
nishitsjsu <nishitajaykumar.doshi@sjsu.edu>
2020-04-16 01:24:29 -0700

    Added logic to fetch results from the provided URL

--- cloudflare-nishit/index.js
+++ cloudflare-nishit/index.js
@@ -6,7 +6,18 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
+
+  const url = 'https://cfw-takehome.developers.workers.dev/api/variants'
+
+  let response = await fetch(url)
+
+  if(response.ok) {
+    console.log("Response")
+    let payload = await response.json()
+    console.log(payload.variants[0], payload.variants[1])
+  }
+
+  return new Response('Response from the provided URL!', {
     headers: { 'content-type': 'text/plain' },
   })
 }
