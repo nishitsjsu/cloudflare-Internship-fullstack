2960754df5d5eaab20039389583300284ae46198
nishitsjsu <nishitajaykumar.doshi@sjsu.edu>
2020-04-16 15:17:23 -0700

    Added documentation for better code readability

--- cloudflare-nishit/index.js
+++ cloudflare-nishit/index.js
@@ -3,6 +3,10 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
+/**
+ * Respond the list of initial URLs
+ * @param url
+ */
 async function getVariantURLs(url){
   let getVariantURLs = await fetch(url)
 
@@ -13,6 +17,11 @@ async function getVariantURLs(url){
   }
 }
 
+/**
+ * Respond with updated HTML Rewriter
+ * @param oldVariant
+ * @returns newVariant
+ */
 async function changeVariant(oldVariant) {
   let newVariant = new HTMLRewriter()
     .on('h1#title', {
@@ -43,8 +52,9 @@ async function changeVariant(oldVariant) {
 
 
 /**
- * Respond with hello worker text
+ * Respond with variant URL in A/B Testing Style
  * @param {Request} request
+ * @returns {Response} customised variant
  */
 async function handleRequest(request) {
   const url = 'https://cfw-takehome.developers.workers.dev/api/variants'
@@ -54,7 +64,8 @@ async function handleRequest(request) {
   if(initialVariantURLs.ok) { 
     variantURLs = await initialVariantURLs.json()
   }
-  
+
+  /* Get last URL index displayed to user from the cookie values */
   if(request.headers.get('Cookie')){
     request.headers.get('Cookie').split(';').forEach(element => {
       let [key,value] = element.split('=')
@@ -62,6 +73,7 @@ async function handleRequest(request) {
     })
   }
 
+  /* Assignment of url : based on cookie value or A/B testing style */
   let currentIndexUrl = (cookieHeader["visitedURL"]) ? cookieHeader["visitedURL"] : Math.floor(Math.random() * 2)
   let currentUrl = await variantURLs.variants[currentIndexUrl]
   let response = await fetch(currentUrl) 
@@ -72,10 +84,12 @@ async function handleRequest(request) {
     let originalResponse = new Response(body, {
       headers: { 'Set-Cookie': `visitedURL = ${currentIndexUrl}` },
     })
-  
+    
+    /* to transform original response to customized response */
     return changeVariant(originalResponse)
 
   } else {
+    /* Error handling : internal server error */
     return new Response("Error in fetching response from variant URL.", {status:500})
   }
 }
